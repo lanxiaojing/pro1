e205110a37e4347a6d66b90ed69204d16528dcec
lanxiaojing <547418453@qq.com>
2015-10-06 18:26:30 +0800

    index.js的变量名修改和注释

--- js/index.js
+++ js/index.js
@@ -1,17 +1,84 @@
-$(document).ready(function(){
-	$(".container").before(
-		"<nav class=\"navbar navbar-inverse navbar-fixed-top\"> " +
-		"<div class=\"container\"> <div class=\"navbar-header\"> " +
-		"<button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapsed\" data-target=\"bs-navbar\" aria-expanded=\"false\"> " +
-		"<span class=\"sr-only\"></span> <span class=\"icon-bar\"></span> <span class=\"icon-bar\"></span>" +
-		"<span class=\"icon-bar\"></span> </button> " +
-		"<a href=\"#\" class=\"navbar-brand\">logo</a> </div> " +
-		"<div class=\"collapse navbar-collapse\" id=\"bs-navbar\"> " +
-		"<ul class=\"nav navbar-nav navbar-right\"> " +
-		"<li><a id=\"homepage\" href=\"index.html\">HOME</a></li> " +
-		"<li><a id=\"signinpage\"  href=\"signIn.html\">注册</a></li> " +
-		"<li><a id=\"signuppage\" href=\"signUp.html\">登录</a></li> " +
-		"</ul> <form action=\"#\" role=\"search\" class=\"navbar-form navbar-right\"> " +
-		"<div class=\"input-group\"> <input type=\"text\" class=\"form-control\" placeholder=\"Search for...\">" +
-		"<span class=\"input-group-btn\"> <button class=\"btn btn-default\" type=\"button\">Go!</button> </span> </div> </form> </div> </div> </nav>");
+$.ajax({//加载index时发请求判断登录否
+	url:"#"+"?tm=" + Math.random(),
+	type:"post",
+	success:function(data){
+		if(data!=""){//返回“”为未登录，返回用户名则已登录
+			welcome(data);//data格式：data=["username":"xx","favorite":14,"follow":4,"message":5]
+		}
+	}
+});
+function welcome(data){//已登录则修改navbar
+	$('.navbar-right>li').remove();
+	$('.navbar-right').append("<div class='navbar-brand pull-right'><a href='#'>"+'欢迎你！'+data.username+"</a></div>");
+	$('#userInfoList>div').remove();
+	$('#userInfoList').append("<a href='#' class='list-group-item'>我的收藏<span class='badge'>"+data.favorite+"</span></a>" +//a链接地址为用户信息界面，未完成
+		                      "<a href='#' class='list-group-item'>我关注的人<span class='badge'>"+data.follow+"</span></a>" +//届时data应加url键
+		                      "<a href='#' class='list-group-item'>我的粉丝<span class='badge'>"+data.fance+"</span> </a>" +
+		                      "<a href='#' class='list-group-item'>私信<span class='badge'>"+data.message+"</span></a>")
+}
+
+$("#hottab").click(function(){//热门的tab点击时发请求
+	tabAjax('hot',#)//#代表请求地址
+});
+$("#friendtab").click(function(){//好友tab
+	tabAjax('friend',##)//##与#是两个不同的phpurl
+});
+function tabAjax(tabContainerId,PHPUrl){
+	$.ajax({
+		url: PHPUrl+"?tm="+Math.random(),
+		type:"post",
+		data: tabContainerId,
+		success:function(data){//data格式为：若为空，则：data=[""];若不为空，则：data=["username":["X","XX","XXXX"],"imgurl":["X,XXX,XXX,XXXX","S,SSS,SS",""],"word":["AAA","QQQQ","WWW"]]
+			var json=eval('('+data+')');//此处还有user头像问题未解决
+			if(json ==""){
+				$("#"+id).append("<div class='alert alert-info' role='alert'>这里什么也没有╮(╯▽╰)╭</div>");
+			}
+			else {
+				tabinner(tabContainerId, json);
+			}
+		}
 	})
+}
+function tabinner(tabContainerId,data){
+	var btnName = '';
+	if(tabContainerId == 'hot'){
+		btnName = "<button type='button' class='btn btn-xs btn-danger pull-right'>"+'加关注'+"</button>";//若非好友动态可将此人加关注
+	}
+	else{
+		btnName =  "<button type='button' class='btn btn-xs btn-info pull-right'>"+'取消关注'+"</button>";//好友可取关
+	}
+	for(i=0;i<data.username.length;i++){
+		html += "<div class='media' style='margin: 20px;'>"
+			+ "<div class='media-left'>"
+			+ "	<a href='#'><img class='img-circle img-responsive' style='width: 50px;height: 50px' src=''></a>"//用户头像问题未解决
+			+ "</div>"
+			+ "<div class='media-body'>"
+			+ "<div class='media-heading'><a href='#'>" +data.username[i] + "</a>" //点击用户名跳转至好友主页功能：是否在data中加入userurl键？
+			+ btnName
+			+ "</div>"
+			+ "<p>"+data.word[i]+"</p>"
+			+ "<div class='center-block'>"
+			+ imgoutput(i)
+			+ "</div>"
+			+ "</div>"
+			+ "</div>"
+			+ "<div class='parting-line'></div>";
+	}
+	function imgoutput(i){
+		var p='';
+		if(data.imgurl[i]!='')
+			if (data.imgurl[i].indexOf(",") >= 0) {
+				var imgUrlArr = [];
+				imgUrlArr =data.imgurl[i].split(",");
+				for (var j = 0; j < imgUrlArr.length; j++) {
+					p += "<img style='width: 30%;height:auto;' src='imgUrlArr[j]'>";//对img显示的样式还可完善，裁图问题待讨论
+				}
+				return p;
+			}
+			else {
+				return p;
+			}
+		else return p;
+	}
+	document.getElementById(tabContainerId).append(html);
+};
